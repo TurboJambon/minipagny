597ff7d2362f63dd3a3326a9b6e62e3261e372ae
TurboJambon <david.chirol@wanadoo.fr>
2017-05-11 12:30:38 +0200

    g pt dans l'echo

--- bin/echo.c
+++ bin/echo.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dchirol <dchirol@student.42.fr>            +#+  +:+       +#+        */
+/*   By: David <David@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/10 18:05:12 by dchirol           #+#    #+#             */
-/*   Updated: 2017/05/10 18:14:35 by dchirol          ###   ########.fr       */
+/*   Updated: 2017/05/11 12:29:56 by David            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putstr_nochar(char *str, char c)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '"')
+			i++;
+		else 
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+	}
+
+}
 
 int main(int ac, char **av)
 {
